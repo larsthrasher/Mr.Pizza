77275b1a00368b1b8fcf3a21dc28d06b59528be3
larsthrasher <thrasherlars@gmail.com>
2021-11-09 19:53:04 -0500

    ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { Navbar } from './Navbar/Navbar';
+import { Banner } from './Banner/Banner';
+import { Menu } from './Menu/Menu';
+import { GlobalStyle } from "./Styles/GlobalStyle";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GlobalStyle/>
+    <Navbar />
+    <Banner />
+    <Menu />
+    </>
   );
 }
 
